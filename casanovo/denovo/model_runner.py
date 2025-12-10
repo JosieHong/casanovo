85c33dc4ab61493fc9a2536a630f945ef12af6c8
Yuhui Hong <josieexception@outlook.com>
2025-12-10 11:12:42 -0800

    Rebase onto v5.1.1

--- casanovo/denovo/model_runner.py
+++ casanovo/denovo/model_runner.py
@@ -374,6 +374,7 @@ class ModelRunner:
         self.model.out_writer = self.writer
 
         test_paths = self._get_input_paths(peak_path, False, "test")
+
         self.writer.set_ms_run(test_paths)
         self.initialize_data_module(test_paths=test_paths)
 
